6cbd779bab24a271469ca4ca6a0c3ccd7a0b4c00
Juan Alday <alday@ieee.org>
2024-05-02 12:09:05 +0200

    Fixed test in climbing_the_leaderboard

--- algorithms/implementation/climbing_the_leaderboard.cpp
+++ algorithms/implementation/climbing_the_leaderboard.cpp
@@ -1,26 +1,31 @@
+// https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 #include <gtest/gtest.h>
 
 #include <algorithm>
 #include <vector>
-#include <unordered_set>
 
 std::vector<int> climbingLeaderboard(std::vector<int> ranked, std::vector<int> player) {
-    std::vector<int> results;
+    std::vector<int> results(player.size(), 0);
 
-    std::set<int, std::greater<int>> data(ranked.begin(), ranked.end());
+    std::sort(ranked.begin(), ranked.end(), std::greater<int>());
+    ranked.erase(std::unique(ranked.begin(), ranked.end()), ranked.end());
+    
 
-    for (auto p : player)
-    {
-        auto [it, inserted] = data.insert(p);
-        results.push_back(1 + static_cast<int>(std::distance(data.begin(), it)));
+    auto resultIter = results.begin();
+    for (int p : player) {
+        // Find the position where player's score fits in the ranked list
+        auto it = std::lower_bound(ranked.begin(), ranked.end(), p, std::greater<int>());
+        // The rank will be the index of iterator + 1 (1-based index)
+        *resultIter++ = std::distance(ranked.begin(), it) + 1;
     }
+
     return results;
 }
 
 TEST(climbingLeaderboard, sample1)
 {
     std::vector output = { 6, 4, 2, 1 };
-    EXPECT_EQ(output, climbingLeaderboard({ 100, 100, 50, 40, 40, 20, 10 }, { 6, 4, 2, 1 }));
+    EXPECT_EQ(output, climbingLeaderboard({ 100, 100, 50, 40, 40, 20, 10 }, { 5, 25, 50, 120 }));
 }
 
 TEST(climbingLeaderboard, sample2)
