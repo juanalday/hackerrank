42ec14193c418cfab028ab104f66288e42855c0f
Juan Alday <alday@ieee.org>
2024-05-02 20:20:20 +0200

    Added insertionSort2

---
+++ algorithms/sorting/insertion_sort2.cpp
@@ -0,0 +1,89 @@
+// https://www.hackerrank.com/challenges/insertionsort2/problem
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
+
+std::vector<std::vector<int>> insertionSort2(std::vector<int> arr) {
+
+	std::vector<std::vector<int>> ret;
+
+	
+	for (auto it1 = next(arr.begin()); it1 != arr.end(); ++it1) {
+		auto i = static_cast<int>(distance(arr.begin(), it1));
+		int current = *it1;
+		
+		int j = i - 1;
+		while (j >= 0 && arr[j] > current) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		
+		arr[j + 1] = current;
+
+
+		ret.push_back(arr);
+	}
+	return ret;
+}
+
+void insertionSort2(int n, std::vector<int> arr) {
+
+	auto vs = insertionSort2(arr);
+	for (auto const & vec : vs)
+	{
+		for_each(vec.begin(), prev(vec.end()), [](int value) {std::cout << value << ' '; });
+		std::cout << vec.back() << "\n";
+	}
+}
+
+TEST(insertionSort2, example)
+{
+	std::vector<std::vector<int>> output{ 
+		{3, 4, 7, 5, 6, 2, 1}, 
+		{3, 4, 7, 5, 6, 2, 1}, 
+		{3, 4, 5, 7, 6, 2, 1}, 
+		{3, 4, 5, 6, 7, 2, 1}, 
+		{2, 3, 4, 5, 6, 7, 1}, 
+		{1, 2, 3, 4, 5, 6, 7} };
+	EXPECT_EQ(output, insertionSort2({3, 4, 7, 5, 6, 2, 1}));
+}
+
+TEST(insertionSort2, sample)
+{
+	std::vector<std::vector<int>> output{ 
+		{1, 4, 3, 5, 6, 2}, 
+		{1, 3, 4, 5, 6, 2}, 
+		{1, 3, 4, 5, 6, 2}, 
+		{1, 3, 4, 5, 6, 2},
+		{1, 2, 3, 4, 5, 6}
+	};
+	EXPECT_EQ(output, insertionSort2({ 1, 4, 3, 5, 6, 2 }));
+}
+
+TEST(insertionSort2, case0)
+{
+	std::vector<std::vector<int>> output{
+		{1, 4, 3, 5, 6, 2},
+		{1, 3, 4, 5, 6, 2},
+		{1, 3, 4, 5, 6, 2},
+		{1, 3, 4, 5, 6, 2},
+		{1, 2, 3, 4, 5, 6}
+	};
+	EXPECT_EQ(output, insertionSort2({ 1, 4, 3, 5, 6, 2 }));
+}
+
+
+TEST(insertionSort2, case5)
+{
+	std::vector<std::vector<int>> const output{ 
+		{7 ,8 ,6 ,5 ,4 ,3 ,2 ,1},
+		{6 ,7 ,8 ,5 ,4 ,3 ,2 ,1},
+		{5 ,6 ,7 ,8 ,4 ,3 ,2 ,1},
+		{4 ,5 ,6 ,7 ,8 ,3 ,2 ,1},
+		{3 ,4 ,5 ,6 ,7 ,8 ,2 ,1},
+		{2 ,3 ,4 ,5 ,6 ,7 ,8 ,1},
+		{1 ,2 ,3 ,4 ,5 ,6 ,7 ,8} };
+	EXPECT_EQ(output, insertionSort2({ 8, 7, 6, 5, 4, 3, 2, 1 }));
+}
