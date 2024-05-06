d7cb10b44c3bd449262cd603947346a50d079b3a
Juan Alday <alday@ieee.org>
2024-05-06 10:14:38 +0200

    Added insertion_sort

--- algorithms/sorting/activity_notifications.cpp
+++ algorithms/sorting/activity_notifications.cpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <algorithm>
+#include <iterator>
 #include <vector>
 
 namespace {
---
+++ algorithms/sorting/insertion_sort.cpp
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/insertion-sort/problem
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
+
+namespace {
+
+	int insertionSort(std::vector<int> arr) {
+		return 0;
+	}
+}
+
+
+TEST(insertionSort, example)
+{
+	EXPECT_EQ(6, insertionSort({4, 3, 2, 1}));
+}
