b2b58eebca29b3bbc081457ce2e60cae10320fac
Juan Alday <alday@ieee.org>
2024-05-15 20:44:07 +0200

    Fixed hash function

--- certificates/problem_solving_intermediate/user_friendly_password_system.cpp
+++ certificates/problem_solving_intermediate/user_friendly_password_system.cpp
@@ -11,23 +11,39 @@
 using ::testing::ElementsAre;
 
 namespace {
-	unsigned long long pow131(auto  n) {
-		constexpr std::array<unsigned long long, 8> pow131 = { 1, 131, 17161, 2248091, 294499921, 38579489651, 5053913144281, 662062621900811};
-		if (n < 8)
-			return pow131[n];
+	constexpr std::array<double, 9> const powersOf131 = {
+		1,                     // 131^0
+		131,                   // 131^1
+		17161,                 // 131^2
+		2248091,               // 131^3
+		294499921,             // 131^4
+		38579489651,           // 131^5
+		5053913144281,         // 131^6
+		662062621900811       // 131^7
+	};
+	constexpr double pow131(auto n) {
+		if (n < 3)
+			return powersOf131[n];
+		return std::pow(131, n);
+	};
 
-		return static_cast<unsigned long long>(std::pow(131, n));
-		};
+	constexpr unsigned long long stagedPow131(char c, auto pos) {
+		return static_cast<unsigned long long>(pow131(pos) * c);
+	}
 
-	int hasherFunc(auto const& value) {
-		auto ret = std::transform_reduce(std::begin(value), std::end(value),
-			0LL,
-			std::plus<>(),
-			[&, len = value.size()](char c) mutable {
-				return (pow131(--len)*c) % 1000000007; 
-			});
+	unsigned long long plainHasherFunc(std::string const& value) {
+		auto len = value.size();
+		unsigned long long ret(0);
+		for (auto x : value)
+		{
+			--len;
+			ret += stagedPow131(x, len);
+		}
+		return ret;
+	}
 
-		return static_cast<int>(ret % 1000000007);
+	int hasherFunc(std::string const& value) {
+		return static_cast<int>(plainHasherFunc(value) % 1000000007);
 	}
 
 	std::pair<int, int> hashPair(std::string str) {
@@ -72,20 +88,56 @@ TEST(UserFriendlyPasswordSystem, pow131)
 	EXPECT_EQ(pow131(5), 38579489651);
 	EXPECT_EQ(pow131(6), 5053913144281);
 	EXPECT_EQ(pow131(7), 662062621900811);
-	EXPECT_EQ(pow131(8), 86730203469006240);
+	EXPECT_EQ(pow131(8), 86730203469006241);
+};
+
+TEST(UserFriendlyPasswordSystem, stagedPow131)
+{
+	EXPECT_EQ(stagedPow131('c', 0), 99);
+	EXPECT_EQ(stagedPow131('c', 1), 12969);
+	EXPECT_EQ(stagedPow131('c', 2), 1698939);
+	EXPECT_EQ(stagedPow131('c', 3), 222561009);
+	EXPECT_EQ(stagedPow131('c', 4), 29155492179);
+	EXPECT_EQ(stagedPow131('c', 5), 3819369475449);
+	EXPECT_EQ(stagedPow131('c', 6), 500337401283819);
+	EXPECT_EQ(stagedPow131('c', 7), 65544199568180288);
+	EXPECT_EQ(stagedPow131('c', 8), 8586290143431617536);
+	EXPECT_EQ(stagedPow131('A', 0), 65);
+	EXPECT_EQ(stagedPow131('A', 1), 8515);
+	EXPECT_EQ(stagedPow131('A', 2), 1115465);
+	EXPECT_EQ(stagedPow131('A', 3), 146125915);
+	EXPECT_EQ(stagedPow131('A', 4), 19142494865);
+	EXPECT_EQ(stagedPow131('r', 0), 114);
+	EXPECT_EQ(stagedPow131('r', 1), 14934);
+	EXPECT_EQ(stagedPow131('r', 2), 1956354);
+	EXPECT_EQ(stagedPow131('r', 3), 256282374);
+	EXPECT_EQ(stagedPow131('r', 4), 33572990994);
+	EXPECT_EQ(stagedPow131('1', 0), 49);
+	EXPECT_EQ(stagedPow131('1', 1), 6419);
+	EXPECT_EQ(stagedPow131('1', 2), 840889);
+	EXPECT_EQ(stagedPow131('1', 3), 110156459);
+	EXPECT_EQ(stagedPow131('1', 4), 14430496129);
 }
 
-TEST(UserFriendlyPasswordSystem, DISABLED_hash) {
-	//auto  c = hashSecret("cAr1");
+TEST(UserFriendlyPasswordSystem, plainHasherFunc) {
+	EXPECT_EQ(plainHasherFunc("c"), 99);
+	EXPECT_EQ(plainHasherFunc("cA"), 13034); // 131*99 + 65 = 12969 + 65 = 13034
+	EXPECT_EQ(plainHasherFunc("cAr"), 1707568); // 17161*99 + 131*65 + 114 = 1699399 + 8515 + 114 = 1707568
+	EXPECT_EQ(plainHasherFunc("cAr1"), 223691457); // 2248091*99 + 17161*65 + 131*114 + 49 = 223691457
+}
+
+TEST(UserFriendlyPasswordSystem, hash) {
 	EXPECT_EQ(hashPair("c").first, 99);
 	EXPECT_EQ(hashPair("cA").first, 13034); // 131*99 + 65 = 12969 + 65 = 13034
 	EXPECT_EQ(hashPair("cAr").first, 1707568); // 17161*99 + 131*65 + 114 = 1699399 + 8515 + 114 = 1707568
+	EXPECT_EQ(hasherFunc("cAr1"), 223691457); // 2248091*99 + 17161*65 + 131*114 + 49 = 223691457
 	EXPECT_EQ(hashPair("cAr1").first, 223691457); // 2248091*99 + 17161*65 + 131*114 + 49 = 223691457
 	EXPECT_EQ(hashPair("cAr1a").first, 303580761); // 294499921*99 + 2248091*65 + 17161*114 + 131*49 + 97 = 303580761
 	EXPECT_EQ(hashPair("cAr1"), std::pair(223691457, 303580664));
 	EXPECT_EQ(hashPair("000A").first, 108738449);
 }
-TEST(UserFriendlyPasswordSystem, DISABLED_example) {
+
+TEST(UserFriendlyPasswordSystem, example) {
 	EXPECT_THAT(authEvents({
 		{"setPassword", "cAr1"},
 		{"authorize", "223691457"},
@@ -96,7 +148,7 @@ TEST(UserFriendlyPasswordSystem, DISABLED_example) {
 	), ElementsAre(1, 1, 0, 1));
 }
 
-TEST(UserFriendlyPasswordSystem, DISABLED_sample0) {
+TEST(UserFriendlyPasswordSystem, sample0) {
 	EXPECT_THAT(authEvents({
 		{"setPassword", "000A"},
 		{"authorize", "108738450"},
@@ -115,7 +167,7 @@ TEST(UserFriendlyPasswordSystem, sample1) {
 	),ElementsAre(0, 0));
 }
 
-TEST(UserFriendlyPasswordSystem, DISABLED_case0) {
+TEST(UserFriendlyPasswordSystem, case0) {
 	EXPECT_THAT(authEvents({
 		{"setPassword", "000A"},
 		{"authorize", "108738450"},
@@ -147,7 +199,7 @@ TEST(UserFriendlyPasswordSystem, case2) {
 	), ElementsAre(1, 1, 1, 1, 1, 1, 0));
 }
 
-TEST(UserFriendlyPasswordSystem, DISABLED_case3) {
+TEST(UserFriendlyPasswordSystem, case3) {
 	EXPECT_THAT(authEvents({
 		{"setPassword", "brnTP3"},
 		{"setPassword", "bnFb"},
