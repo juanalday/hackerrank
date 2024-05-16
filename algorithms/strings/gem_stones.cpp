383a5148ec675f52277f39513708cf9a5e73b7ce
Juan Alday <alday@ieee.org>
2024-05-16 17:36:42 +0200

    Added gem_stones.cpp

---
+++ algorithms/strings/gem_stones.cpp
@@ -0,0 +1,51 @@
+// https://www.hackerrank.com/challenges/gem-stones/problem
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+namespace {
+	int gemstones(std::vector<std::string> arr) {
+		std::vector<int> gem(26, 0);
+		std::vector<int> single(gem);
+		for (auto const& s : arr) {
+			std::fill(single.begin(), single.end(), 0);
+			std::for_each(s.begin(), s.end(), [&single](auto c) { single[c - 'a'] = 1; });
+			std::transform(gem.begin(), gem.end(), single.begin(), gem.begin(), std::plus<int>());
+		}
+		return std::count(gem.begin(), gem.end(), arr.size());
+	}
+}
+
+TEST(GemStones, example) {
+	EXPECT_EQ(2, gemstones({ "abc", "abc", "bc" }));
+}
+
+TEST(GemStones, sample0) {
+	EXPECT_EQ(2, gemstones({ "abcdde", "baccd", "eeabg"}));
+}
+
+TEST(GemStones, sample1) {
+	EXPECT_EQ(4, gemstones({ "basdfj", "asdlkjfdjsa", "bnafvfnsd", "oafhdlasd" }));
+
+}
+
+TEST(GemStones, sample2) {
+	EXPECT_EQ(0, gemstones({ "vtrjvgbj", "mkmjyaeav", "sibzdmsk" }));
+}
+
+TEST(GemStones, case27) {
+	EXPECT_EQ(2, gemstones({ "abcdde", "baccd", "eeabg" }));
+}
+
+TEST(GemStones, case28) {
+	EXPECT_EQ(4, gemstones({ "basdfj", "asdlkjfdjsa", "bnafvfnsd", "oafhdlasd" }));
+
+}
+
+TEST(GemStones, case29) {
+	EXPECT_EQ(0, gemstones({ "vtrjvgbj", "mkmjyaeav", "sibzdmsk" }));
+}
+
