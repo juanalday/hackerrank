22a89f6b69d6c639bd5dfa85271b7fef9f2c7944
Juan Alday <alday@ieee.org>
2024-05-23 19:20:59 +0200

    Fixing tests for bear_and_steady_gene

--- algorithms/strings/bear_and_steady_gene.cpp
+++ algorithms/strings/bear_and_steady_gene.cpp
@@ -3,56 +3,47 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <numeric>
 #include <string>
+#include <utility>
 
 namespace {
 
 	int steadyGene(std::string const& gene) {
-		
 		auto stableCount = gene.size() / 4; // Once we have this many of each letter, we have a stable gene
 
 		std::map<char, int> geneCount;
 		std::for_each(gene.begin(), gene.end(), [&geneCount](char c) { geneCount[c]++; });
-		// If we already have a stable gene, we can return 0
-		if (std::all_of(geneCount.begin(), geneCount.end(), [stableCount](auto const& p) { return p.second == stableCount; })) {
-			return 0;
-		}
-		// Otherwise we need to find the smallest substring that contains all the letters
-		// We can do this by using a sliding window
-		size_t left{ 0 }, right{ 0 };
-		auto leftIter = gene.begin();
-		auto rightIter = gene.begin();
-		auto minLen{ gene.size() }; // Initialize the window
-		while (right < gene.size()) {
-			// Expand the window
-			// Move the right pointer until we have a stable gene
-			geneCount[gene[right]]--;
-			++right;
-			++rightIter;
+		auto isStable = [&]() {return std::all_of(geneCount.begin(), geneCount.end(), [stableCount](auto const& p) { return p.second <= stableCount; }); };
 
-			// Move the left pointer until we no longer have a stable gene
-			while (std::all_of(geneCount.begin(), geneCount.end(), [stableCount](auto const& p) { return p.second <= stableCount; })) {
-				minLen = std::min(minLen, right - left + 1);
-				geneCount[gene[left]]++;
-				left++;
+		// If we already have a stable gene, we can return 0
+		if (isStable())
+			return 0;
+
+		int minLen = INT_MAX;
+		auto leftIter = gene.begin();
+		for (auto rightIter = gene.begin(); rightIter != gene.end(); ++rightIter) {
+			--geneCount[*rightIter];
+
+			while (isStable())
+			{
+				minLen = std::min(minLen, static_cast<int>(std::distance(leftIter, rightIter) + 1));
+				++geneCount[*leftIter];
 				++leftIter;
 			}
 		}
-
-
 		return minLen;
-
 	}
 }
 
-TEST(BearAndSteadyGene, DISABLED_example) {
-	EXPECT_EQ(3, steadyGene("ACTGAAAG"));
+TEST(BearAndSteadyGene, example) {
+	EXPECT_EQ(2, steadyGene("ACTGAAAG"));
 }
 
-TEST(BearAndSteadyGene, DISABLED_sample0) {
+TEST(BearAndSteadyGene, sample0) {
 	EXPECT_EQ(5, steadyGene("GAAATAAA"));
 }
 
-TEST(BearAndSteadyGene, DISABLED_sample1) {
-	EXPECT_EQ(0, steadyGene("TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC"));
+TEST(BearAndSteadyGene, sample1) {
+	EXPECT_EQ(5, steadyGene("TGATGCCGTCCCCTCAACTTGAGTGCTCCTAATGCGTTGC"));
 }
